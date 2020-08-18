e3a3ec896d0ac1b96ded857b94817678ea32af93
RIGUANG JIN <bosco@RIGUANGs-iMac.local>
2020-08-18 14:10:15 -0400

    auth middleware

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+
 const config = require('./config/key');
 
 
 const { User } = require('./models/user');
+const { auth } = require('./middleware/auth')
 const user = require('./models/user');
 
 mongoose.connect(config.mongoURI,
@@ -23,6 +25,22 @@ mongoose.connect(config.mongoURI,
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+
+
+
+// 
+app.get('/api/user/auth', auth, (req, res) => {
+    res.status(200).json({
+        _id: req._id,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role
+    })
+})
+
 
 
 // route
@@ -43,22 +61,26 @@ app.post('/api/user/login', (req, res) => {
     User.findOne({ email: req.body.email }, (err, user) => {
         if(!user) 
         return res.json({ loginSuccess: false, message: " Auth failed, email not found"})
+
+        // password
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch){
+                return res.json({ loginSuccess: false, message: " wrong password" })
+            }
+
+            // token
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess : true })
+            })
+
+        });
+
     });
 
-    // password
-    user.comparePassword(req.body.password, (err, isMatch) => {
-        if(!isMatch){
-            return res.json({ loginSuccess: false, message: " wrong password" })
-        }
-    })
 
-    // token
-    user.generateToken((err, user) => {
-        if(err) return res.status(400).send(err);
-        res.cookie("x_auth", user.token)
-            .status(200)
-            .json({ loginSuccess : true })
-    })
 })
 
 app.listen(5000);
--- models/user.js
+++ models/user.js
@@ -72,6 +72,18 @@ userSchema.methods.generateToken = function(cb){
     })
 }
 
+userSchema.static.findByToken = function(token, cb) {
+    var user = this;
+
+    jwt.verify(token, 'secret', function(err, decode) {
+        user.findOne({ "_id": decode, "token": token }, function(err, user) {
+            if(err) return cb(err);
+            cb(null, user);
+        })
+    })
+}
+
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
