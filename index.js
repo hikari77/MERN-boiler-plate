caeaf62cd05abca26a85cbc994d018e6f8ee39f9
RIGUANG JIN <bosco@RIGUANGs-iMac.local>
2020-08-18 08:22:04 -0400

    login implt start

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const config = require('./config/key');
 
 
 const { User } = require('./models/user');
+const user = require('./models/user');
 
 mongoose.connect(config.mongoURI,
     {
@@ -27,17 +28,37 @@ app.use(cookieParser());
 // route
 app.post('/api/users/register', (req, res) => {
     const user = new User(req.body);
-    
+
     user.save((err, userData) => {
         if(err) return res.json({ success: false.err })
     })
     return res.status(200)
 })
 
-app.get('/', (req, res) => {
-    res.json({"hi":  "hihi=="})
-})
+// login
+app.post('/api/user/login', (req, res) => {
 
+    // email
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if(!user) 
+        return res.json({ loginSuccess: false, message: " Auth failed, email not found"})
+    });
+
+    // password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+        if(!isMatch){
+            return res.json({ loginSuccess: false, message: " wrong password" })
+        }
+    })
+
+    // token
+    user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+        res.cookie("x_auth", user.token)
+            .status(200)
+            .json({ loginSuccess : true })
+    })
+})
 
 app.listen(5000);
 
