0c4b168594589782392772f49689699c5f318854
RIGUANG JIN <bosco@RIGUANGs-iMac.local>
2020-08-18 20:01:52 -0400

    setup route

---
+++ client/src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import About from './about'
+
+
+
+
+function App() {
+  return (
+    <div >
+      <Switch>
+        {/* <Route path="/" component={Home}/> */}
+        <Route path="/about" component={About}/>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
